ae20c259d54130c6f0f804cba05d6cb1d67fa87a
Scott <sco.suen.lab@gmail.com>
2016-04-02 15:52:26 -0400

    feat:initialize project

---
+++ learning-springsession/src/main/java/com/scott/learning_springsession/App.java
@@ -0,0 +1,9 @@
+package com.scott.learning_springsession;
+
+import org.springframework.boot.SpringApplication;
+
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfiguration.class);
+	}
+}
---
+++ learning-springsession/src/main/java/com/scott/learning_springsession/SpringConfiguration.java
@@ -0,0 +1,10 @@
+package com.scott.learning_springsession;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@SpringBootApplication
+@EnableRedisHttpSession
+public class SpringConfiguration {
+
+}
