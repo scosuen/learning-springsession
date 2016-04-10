6330855c9d9f53cc19ef9063606a38c97e6dd953
Scott <sco.suen.lab@gmail.com>
2016-04-10 10:24:00 -0400

    feat: more examples.

--- learning-springsession/src/main/java/com/scott/learning_springsession/SpringConfiguration.java
+++ learning-springsession/src/main/java/com/scott/learning_springsession/SpringConfiguration.java
@@ -1,10 +1,25 @@
 package com.scott.learning_springsession;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 
 @SpringBootApplication
 @EnableRedisHttpSession
 public class SpringConfiguration {
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(final RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+		redisTemplate.setConnectionFactory(connectionFactory);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+		return redisTemplate;
+	}
 
 }
--- learning-springsession/src/main/java/com/scott/learning_springsession/controller/UserController.java
+++ learning-springsession/src/main/java/com/scott/learning_springsession/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/user/")
 public class UserController {
-	
+
 	@RequestMapping(value = "/first", method = RequestMethod.GET)
 	public Map<String, Object> firstResp(HttpServletRequest request) {
 		Map<String, Object> map = new HashMap<>();
-		request.getSession().setAttribute("request Url", request.getRequestURL());
 		map.put("request Url", request.getRequestURL());
+		request.getSession().setAttribute("map", map);
 		return map;
 	}
 
@@ -26,6 +27,27 @@ public class UserController {
 		Map<String, Object> map = new HashMap<>();
 		map.put("sessionId", request.getSession().getId());
 		map.put("message", request.getSession().getAttribute("map"));
+
+		return map;
+	}
+
+	@RequestMapping(value = "/login/{userName}/{device}", method = RequestMethod.GET)
+	public String login(HttpServletRequest request, @PathVariable("userName") String userName, @PathVariable("device") String device) {
+		Map<String, Object> map = new HashMap<>();
+		request.getSession().setAttribute("userName", userName);
+		request.getSession().setAttribute("device", device);
+		return "userName:" + userName + ", device:" + device;
+	}
+
+	@RequestMapping(value = "/validate/{userName}/{device}", method = RequestMethod.GET)
+	public Object validate(HttpServletRequest request, @PathVariable("userName") String userName, @PathVariable("device") String device) {
+		if (request.getSession(false) == null)
+			return "session expired";
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("sessionId", request.getSession(false).getId());
+		map.put("userName", request.getSession(false).getAttribute("userName"));
+		map.put("device", request.getSession(false).getAttribute("device"));
 		return map;
 	}
 }
